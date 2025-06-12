a2f8c324d8150745b85cb3accc81860888b1ac30
jcentenoClicklease <jcenteno@clicklease.com>
2025-06-12 22:32:33 +0000

    Adding setup.sh from the codespace!

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-git add setup.sh --chmod=+x
-git commit -m "Adding setup.sh from the codespace!"
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install sl
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
